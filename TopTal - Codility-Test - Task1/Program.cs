65e7bafd6e980205baa56177d28357eefa30ef89
Doc Rizwan <Rizwan.Doc362436@gmail.com>
2016-12-01 22:26:54 +0500

    Initial Commit.

---
+++ TopTal - Codility-Test - Task1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopTal___Codility_Test___Task1
+{
+	class Program
+	{
+
+		public static void Main(string[] args)
+		{
+
+			Program obj = new Program();
+
+			//int x = obj.CountCharacterOccurrences("(((())((", ')', 0, "(((())((".Length);
+			//int x = obj.CountCharacterOccurrences("(((())((", ')', 0, "(((())((".Length);
+			//
+			//int x = obj.FindingKValue("(())))(");
+			int x = obj.FindingKValue("((()(()))(()()()()))(");
+			Console.WriteLine(x);
+			Console.ReadKey();
+		}
+		public int FindingKValue(string S)
+		{
+			for (int k = 1; k < S.Length; k++)
+			{
+				int openingBs = CountCharacterOccurrences(S, '(', 0, k - 1);
+				int x = S.Length - k;
+				int closingBs = CountTrailingCharacterOccur(S, ')', k, S.Length - 1);
+				if (openingBs == closingBs)
+				{
+					return k;
+				}
+			}
+			return -1;
+		}
+		//public bool ValidateInputString()
+		//{
+
+		//}
+
+		public int CountCharacterOccurrences(string Input, char Char, int StartIndex, int LastIndex)
+		{
+			int charCount = 0;
+			for (int i = StartIndex; i <= LastIndex; i++)
+			{
+				if (Input.ElementAt(i) == Char)
+				{
+					charCount++;
+				}
+			}
+			return charCount;
+		}
+		public int CountTrailingCharacterOccur(string input, char Char, int start, int last)
+		{
+			int count = 0;
+			/*
+			for (int i=last; i >= start; i--)
+			{
+				if(input.ElementAt(i) == Char)
+				{
+					count++;
+				}
+
+			}
+			*/
+			for (int i = start; i <= last; i++)
+			{
+				if (input.ElementAt(i) == Char)
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+
+	}
+}
+
---
+++ TopTal - Codility-Test - Task2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopTal___Codility_Test___Task2
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			//Console.Write(Convert.ToString(9, -2));
+			Program obj = new Program();
+			//Console.Write(obj.ConvertToBase10(new int[] { 1,0,0,1,1 }));
+			//Console.Write(obj.ConvertToBase10(new int[] { 1, 0, 0, 1, 1, 1 }));
+
+			//Console.Write(obj.ConvertToNegativeBase2(-9));
+			//Console.Write(obj.ConvertToNegativeBase2(23));
+
+			//var output = obj.NegativeBase2NumberNegation(new int[] { 1, 0, 0, 1, 1 }); // X = 9
+
+			var output = obj.NegativeBase2NumberNegation(new int[] { 1, 0, 0, 1, 1, 1 }); // X = -23
+			Console.WriteLine(output);
+			Console.ReadKey();
+
+		}
+		public int[] NegativeBase2NumberNegation(int[] input)
+		{
+			int inp = ConvertToBase10(input);
+
+			return ConvertToNegativeBase2((-1) * inp);
+		}
+
+		public int[] ConvertToNegativeBase2(int NegNum)
+		{
+			List<int> NegBaseNum = new List<int>();
+
+			while (NegNum != 0)
+			{
+				int remainder = NegNum % -2;
+				NegNum = NegNum / -2;
+
+				if (remainder < 0)
+				{
+					remainder += 2;
+					NegNum += 1;
+				}
+
+				Console.WriteLine(remainder);
+				NegBaseNum.Add(remainder);
+			}
+			return NegBaseNum.ToArray();
+		}
+		public int ConvertToBase10(int[] input)
+		{
+			int sum = 0;
+			for (int i = 0; i < input.Length; i++)
+			{
+				sum += input[i] * (int)Math.Pow(-2, i);
+			}
+
+
+			return sum;
+		}
+
+	}
+
+}
